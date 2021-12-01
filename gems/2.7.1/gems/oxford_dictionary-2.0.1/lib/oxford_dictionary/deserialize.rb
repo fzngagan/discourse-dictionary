ff582e91dacb19a4415d1bfebf17a3cdced4ce40
Faizaan Gagan <fzngagan@gmail.com>
2021-12-01 23:52:55 +0530

    removed gems folder

--- gems/2.7.1/gems/oxford_dictionary-2.0.1/lib/oxford_dictionary/deserialize.rb
+++
@@ -1,9 +0,0 @@
-require 'json'
-
-module OxfordDictionary
-  class Deserialize
-    def call(payload)
-      JSON.parse(payload, object_class: OpenStruct)
-    end
-  end
-end
--- gems/2.7.1/gems/plissken-0.1.0/lib/plissken.rb
+++
@@ -1,1 +0,0 @@
-require File.dirname(__FILE__) + '/plissken/ext/hash/to_snake_keys'
